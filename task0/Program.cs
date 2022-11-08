607420e69e76cc6249766af4160637e83409fd10
diana <volchenko_93@mail.ru>
2022-11-08 17:22:47 +0300

    first part of homework

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Программа, которая из имеющегося массива строк формирует
+массив из строк, длина которых меньше либо равна 3.
+*/
+
+int findLengthOfNewArray(string[] array)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            j++;
+        }
+    }
+    return j;
+}
+
+string[] findLessThenThreeSimbols(string[] someArray , int j)
+{
+    string[] array = new string[j];
+    for (int i = 0; i < someArray.Length; i++)
+    {
+        if (someArray[i].Length <= 3)
+        {
+            for (int n = 0; n < j; n++)
+            {
+                array[n] = someArray[i];   
+            }
+        }
+    }
+    return array;
+}
+
+string[] array = { "hello", "2", "world", ";-)"};
+int length = findLengthOfNewArray(array);
+string[] result = findLessThenThreeSimbols(array, length);
+foreach (var value in  result) {
+    Console.Write(value + " ");
+}
+//Console.WriteLine(length);
